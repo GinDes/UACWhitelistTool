b7524df6237af5dc8992f0c0ffed74385b309b64
GinDes <130630923+GinDes@users.noreply.github.com>
2024-03-05 01:02:38 +0800

    Update Form.cs

--- Form.cs
+++ Form.cs
@@ -119,7 +119,8 @@ namespace UAC白名单小工具
         // 监视输入框
         private void TextBox_程序名称_TextChanged(object sender, EventArgs e)
         {
-            TextBox_程序名称.Text = Regex.Replace(TextBox_程序名称.Text, @"[^\u4e00-\u9fa5_a-zA-Z0-9\.]", "");
+            //TextBox_程序名称.Text = Regex.Replace(TextBox_程序名称.Text, @"[^\u4e00-\u9fa5_a-zA-Z0-9\.]", "");
+            TextBox_程序名称.Text = Regex.Replace(TextBox_程序名称.Text, @"[<>:/\\|*?\""]", "");;
             //Debug.Print(TextBox_程序名称.Text);
             if (TextBox_程序名称.Text != "")
             {
